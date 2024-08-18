50d20cd255ed59adb05714a7b729ff4923c3488e
sangmin1120 <sangmin8276@gmail.com>
2024-08-18 14:56:29 +0900

    feat: order 객체,역할,구현

---
+++ spring강의/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findByiId(memberId);
+        int discountPrice = discountPolicy.discount(member,itemPrice);
+
+        return new Order(memberId,itemName,itemPrice,discountPrice);
+    }
+}
