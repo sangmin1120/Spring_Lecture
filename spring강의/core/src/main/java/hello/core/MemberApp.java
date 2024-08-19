329beb1d2d817f3eeb619593b847028ca03fd4ac
sangmin1120 <sangmin8276@gmail.com>
2024-08-19 12:31:25 +0900

    순수 자바 test code

--- spring강의/core/src/main/java/hello/core/MemberApp.java
+++ spring강의/core/src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.Member;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = new MemberServiceImpl();
+       // spring
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 메서드 이름 , 반환 type
+
         Member member = new Member(1L, "memberA", Grade.Vip);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member);
-        System.out.println("find Member = " + findMember);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
