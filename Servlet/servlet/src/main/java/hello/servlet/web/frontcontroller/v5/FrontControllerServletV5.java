ffedafd8d682cdb05a30ae6c4580e5da8ebb47fe
sangmin1120 <sangmin8276@gmail.com>
2024-09-26 17:42:08 +0900

    feat: V4 추가

--- Servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ Servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,7 +6,11 @@ import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
@@ -33,18 +37,29 @@ public class FrontControllerServletV5 extends HttpServlet {
     }
 
     private void initHandlerMappingMap() {
+        // V3
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form",new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save",new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members",new MemberListControllerV3());
+
+        // V4 추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form",new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save",new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members",new MemberListControllerV4());
     }
     private void initHandlerAdapters() {
+        // V3
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+
+        // V4 추가
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // System.out.println("FrontControllerServletV5 service");
 
+        // 1. handler 찾기
         Object handler = getHandler(request);
 
         if (handler == null){
@@ -52,13 +67,17 @@ public class FrontControllerServletV5 extends HttpServlet {
             return;
         }
 
+        // 2. adapter 찾기
         MyHandlerAdapter adapter = getHandlerAdapter(handler);
 
+        // 3. adapter를 이용해 모델 등록
         ModelView mv = adapter.handle(request, response, handler);
 
+        // 4. URI 주소 찾기
         String viewName = mv.getViewName(); // 논리 이름
         MyView view = viewResolver(viewName); // 실제 위치 반환
 
+        // 5. 렌더링
         view.render(mv.getModel(),request,response);
 
     }
