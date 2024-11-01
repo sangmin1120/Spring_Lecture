fcdb850892704d170f0f8b04443a83f6dca6f56e
sangmin1120 <sangmin8276@gmail.com>
2024-11-01 14:29:54 +0900

    chore: 오타 수정

--- Servlet/servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ Servlet/servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.mvc.Controller;
 public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("OldCOntroller.handleRequest");
+        System.out.println("OldController.handleRequest");
         return new ModelAndView("new-form");
     }
 }
