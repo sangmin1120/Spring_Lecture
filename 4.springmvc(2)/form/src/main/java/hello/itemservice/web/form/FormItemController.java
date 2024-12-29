15a289c1623a194664776665c1b43572018b4fd7
sangmin1120 <sangmin8276@gmail.com>
2024-12-29 17:58:39 +0900

    스프링 Form - Thymeleaf 연습

--- 4.springmvc(2)/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ 4.springmvc(2)/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,21 +1,47 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    // model 자동으로 담기게 됨
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String,String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+        return regions;
+    }
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST","빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL","일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW","느린 배송"));
+        return deliveryCodes;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -39,6 +65,11 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        log.info("item.open={}",item.getOpen());
+        log.info("item.regions={}",item.getRegions());
+        log.info("item.itemTpye={}",item.getItemType());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
